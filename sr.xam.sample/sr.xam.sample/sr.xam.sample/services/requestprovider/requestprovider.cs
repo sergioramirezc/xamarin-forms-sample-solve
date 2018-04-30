1c23883e593f4f7f247ecf2e1c2e7eea80fe5aeb
Sergio Ramirez <sergioramirez@outlook.com>
2018-04-30 06:08:10 -0500

    app center

---
+++ sr.xam.sample/sr.xam.sample/sr.xam.sample/services/requestprovider/requestprovider.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System;
+using SR.Xam.Sample.Services;
+using System.Collections.Generic;
+using Microsoft.AppCenter.Crashes;
+
+namespace SR.Xam.Sample.Services.RequestProvider
+{
+    public class RequestProvider : IRequestProvider
+    {
+        private static HttpClient instance;
+        private IDialogService _dialogservice;
+
+        public RequestProvider(IDialogService dialogservice)
+        {
+            _dialogservice = dialogservice;
+        }
+
+        public async Task<TResult> GetAsync<TResult>(string uri, string token = "")
+        {
+            try
+            {
+                HttpClient httpClient = CreateHttpClient(token);
+                HttpResponseMessage response = await httpClient.GetAsync(uri);
+
+                string serialized = await response.Content.ReadAsStringAsync();
+
+                TResult result = await Task.Run(() => JsonConvert.DeserializeObject<TResult>(serialized));
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                await _dialogservice.ShowAlertAsync(uri + " " + ex.Message, "Error", "Aceptar");
+                var properties = new Dictionary<string, string> {
+                    { "Url", uri },
+                    { "Method", "Get" },
+                };
+                Crashes.TrackError(ex, properties);
+
+                return default(TResult);
+            }
+        }
+
+        private HttpClient CreateHttpClient(string token = "")
+        {
+            var httpClient = GetInstance();
+
+            if (!string.IsNullOrEmpty(token))
+            {
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+            return httpClient;
+        }
+
+        private HttpClient GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new HttpClient();
+                instance.Timeout = TimeSpan.FromSeconds(30);
+                instance.MaxResponseContentBufferSize = 256000;
+                instance.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            }
+
+            return instance;
+        }
+    }
+}
